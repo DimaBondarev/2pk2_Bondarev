f2cc99bfa7159c10ae7fe5b8b722cd583aadfc91
Vid Dima <aseg55466@gmail.com>
2023-09-16 07:55:48 +0300

    Добавлена 3 практика

---
+++ pz_03/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace pz_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 11:");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            string message;
+
+            switch (k)
+            {
+                case 1:
+                    message = "Привет, первоклассник";
+                    break;
+                case 2:
+                    message = "Привет, второклассник";
+                    break;
+                case 3:
+                    message = "Привет, третьеклассник";
+                    break;
+                case 4:
+                    message = "Привет, четвероклассник";
+                    break;
+                case 5:
+                    message = "Привет, пятиклассник";
+                    break;
+                case 6:
+                    message = "Привет, шестиклассник";
+                    break;
+                case 7:
+                    message = "Привет, седьмиклассник";
+                    break;
+                case 8:
+                    message = "Привет, восьмиклассник";
+                    break;
+                case 9:
+                    message = "Привет, девятиклассник";
+                    break;
+                case 10:
+                    message = "Привет, десятиклассник";
+                    break;
+                case 11:
+                    message = "Привет, одиннадцатиклассник";
+                    break;
+                default:
+                    message = "Некорректное число";
+                    break;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
