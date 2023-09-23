52b832e989551c84c565adb97964b15797802b39
DimaBondarev <aseg55466@gmail.com>
2023-09-23 13:17:06 +0300

    Добавление пз 5

---
+++ pz_05/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace pz_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6; // заданное число
+
+            int fibonacci = Fibonacci(n);
+
+            Console.WriteLine(fibonacci);
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            int a = 0;
+            int b = 1;
+            int result = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+
+            return result;
+        }
+    }
+}
