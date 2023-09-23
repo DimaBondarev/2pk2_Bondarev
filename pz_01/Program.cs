bb5a20e9dec5af6bbe031e3117b0cba115530d94
DimaBondarev <aseg55466@gmail.com>
2023-09-23 13:11:06 +0300

    добавление пз 1

---
+++ pz_01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = -1;
+            int c = 4;
+            double d = Math.Pow(10, -3);
+            double f = Math.Tan(-8);
+            double s = a - c;
+            double g = Math.Pow(a, 2) + Math.Pow(b, 2);
+            double h = s * g;
+            double j = Math.Pow(2, a) + Math.Pow(b, 2) - 2.2 * c;
+            double k = Math.Pow(j, 1 / 3);
+            double l = Math.Cos(2 * a) / Math.Sin(5);
+            double Rez = d * f - (h / k) - l;
+
+            { Console.WriteLine(Rez); }
+        }
+    }
+}
