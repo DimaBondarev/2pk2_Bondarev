d69f65e2bfd87405fce0eb08606cc31b63b2c16c
Vid Dima <aseg55466@gmail.com>
2023-11-30 23:21:55 +0500

    добавлена пз 10

---
+++ PZ_10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://example.com/home/";
+            string domainWithTrailingSlash = url.Replace("https://", "").Replace("http://", ""); 
+            domainWithTrailingSlash = domainWithTrailingSlash.TrimEnd('/'); 
+            string[] domainParts = domainWithTrailingSlash.Split('.');
+            string topLevelDomain = domainParts[^1]; 
+            Console.WriteLine($"Домен верхнего уровня: {topLevelDomain}");
+        }
+    }
+}
