502c232a4d1080c74ccf9a46b42f50f25dd051e7
Vid Dima <aseg55466@gmail.com>
2023-12-23 00:20:29 +0500

    добавление новой пз 12

---
+++ pz_12novoe/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_12novoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку символов:"); // Вывод на экран просьбы ввести строку символов
+            string input = Console.ReadLine();             // Чтение введенной пользователем строки
+            char[] charArray = input.ToCharArray();        // Преобразование введенной строки в массив символов
+            char mostFrequentChar = FindMostFrequentChar(charArray);                       // Вызов метода для нахождения наиболее часто встречающегося символа
+            Console.WriteLine($"Наиболее часто встречающийся символ: {mostFrequentChar}"); // Вывод информации о наиболее часто встречающемся знаке
+        }
+        static char FindMostFrequentChar(char[] charArray)                // Метод для нахождения наиболее часто встречающегося символа в массиве
+        {
+            var groupedChars = charArray.GroupBy(c => c)
+            .Select(g => new { Char = g.Key, Count = g.Count() });          // Группировка символов и подсчет их встречаемости
+            var mostFrequentChar = groupedChars.OrderByDescending(x => x.Count).First().Char;  // Поиск символа с максимальной встречаемостью
+            return mostFrequentChar;                                        // Возвращение найденного наиболее часто встречающегося символа
+        }
+    }
+}
